2851d74e62dc9364637fd998830ec28f72ef2fad
Shaik Arshad <138425231+arshad7658@users.noreply.github.com>
2025-05-07 13:57:35 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 def predictions(img_path):
-    state_dict = torch.load('trained_weights/best_model_weights3.pth')
+    state_dict = torch.load('model.pth')
     model.load_state_dict(state_dict)
     model.eval()
-    input_img=cv2.imread(img_path)
-    img =cv2.resize(input_img, (224, 224))
+    # input_img=cv2.imread(img_path)
+    img =cv2.resize(img_path, (224, 224))
     img = torch.tensor(img).permute([2,0,1])
     im = NORMALIZE(img/255.0)
     im = im[None]
@@ -19,8 +19,8 @@ def predictions(img_path):
     gender = gender.to('cpu').detach().numpy()
     age = (age.to('cpu').detach().numpy())
     
-    pred_gender = np.where(gender[0][0]<0.5, 'Male', 'Female')
-    pred_age = int(age[0][0]*116*(10**12))                           
+    pred_gender = 'Male' if int(gender[0][0]>0.5) == 1 else 'Female'
+    pred_age = round(age[0][0]*80)                           
     
 
     return pred_gender,pred_age
